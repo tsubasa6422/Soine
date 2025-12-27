c1a473e6427c802eab74a5d7f8c8144fd1acc9df
Tsubasa Yamada <220030647+tsubasa6422@users.noreply.github.com>
2025-12-27 15:04:25 +0000

    構文エラー修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,30 +6,25 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
 
-    ids = Array(params.dig(:post, :child_ids)).reject(&:blank?).map(&:to_i)
-    child_ids = current_user.children.where(id: ids).pluck(:id)
+    child_ids = selected_child_ids
 
     if @post.save
       child_ids.each do |cid|
-        @post.child_posts.create!(child_id: cid)  
+        @post.child_posts.create!(child_id: cid)
       end
-
       redirect_to mypage_path(filter: params[:filter]), notice: "投稿しました。"
     else
-      ...
+      render :new
     end
   end
 
-
   def show
     @comment = Comment.new
-
     @comments = @post.comments
-                    .where(parent_id: nil)
-                    .includes(:user, replies: :user)
-                    .order(created_at: :desc)
+                     .where(parent_id: nil)
+                     .includes(:user, replies: :user)
+                     .order(created_at: :desc)
   end
-
 
   def index
     @q = params[:q].to_s.strip
@@ -46,26 +41,23 @@ class PostsController < ApplicationController
   def update
     @post.assign_attributes(post_params)
 
-    ids = Array(params.dig(:post, :child_ids)).reject(&:blank?).map(&:to_i)
-    child_ids = current_user.children.where(id: ids).pluck(:id)
+    child_ids = selected_child_ids
 
     if @post.save
       @post.child_posts.destroy_all
-      child_ids.each { |cid| @post.child_posts.create!(child_id: cid) }
-
+      child_ids.each do |cid|
+        @post.child_posts.create!(child_id: cid)
+      end
       redirect_to post_path(@post), notice: "投稿を更新しました。"
     else
       render :edit
     end
   end
 
-
   def destroy
     @post.destroy
     redirect_to mypage_path, notice: "投稿を削除しました。"
   end
-
-  
 
   private
 
@@ -74,15 +66,16 @@ class PostsController < ApplicationController
   end
 
   def authorize_post!
-    redirect_to mypage_path(filter: "timeline"), alert: "権限がありません。" unless @post.user == current_user
+    return if @post.user == current_user
+    redirect_to mypage_path(filter: "timeline"), alert: "権限がありません。"
   end
 
   def post_params
     params.require(:post).permit(:title, :body, :area_id, images: [], child_ids: [])
   end
 
-  def limit_child_ids_to_current_user(post)
+  def selected_child_ids
     ids = Array(params.dig(:post, :child_ids)).reject(&:blank?).map(&:to_i)
-    post.child_ids = current_user.children.where(id: ids).pluck(:id)
+    current_user.children.where(id: ids).pluck(:id)
   end
 end
