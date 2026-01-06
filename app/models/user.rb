dbfb0c56799139cf4c0d65fdf848b0f1ba3cdb9b
Tsubasa Yamada <220030647+tsubasa6422@users.noreply.github.com>
2026-01-06 12:33:19 +0000

    管理者機能不具合修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
-  has_many :child_posts, dependent: :destroy
+ # has_many :child_posts, dependent: :destroy
 
 
   def get_profile_image(width, height)
